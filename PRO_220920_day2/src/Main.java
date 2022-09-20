41950629cb5a88263c268be78ca8a29a8cbda19c
DESKTOP-F1SR0K1\Sangjin Kim <demonicksj@gmail.com>
2022-09-20 10:53:28 +0900

    재귀함수 시작

---
+++ PRO_220920_day2/src/Main.java
@@ -0,0 +1,58 @@
+/*
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
+*/
+
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
+
+//Quiz N을 입력받으면 1~N까지 입력하는 프로그램
+/*
+import java.util.*;
+import java.io.*;
+
+public class Main {
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static StringTokenizer st;
+	static int N;
+	static int DE;
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+
+		N=Integer.parseInt(br.readLine());
+		
+		f_quiz(1);
+
+	}
+	
+	static void f_quiz(int n) {
+		if(n>N) {
+			return;
+		}
+		DE=12;
+		
+		System.out.println(n);
+		f_quiz(n+1);
+		
+	}
+
+}
+*/
